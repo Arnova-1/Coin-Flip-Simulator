4bca783a72a0b31b7246cf48e154c7821d34821c
Arnova-1 <arnova124@gmail.com>
2025-10-28 09:18:20 +0700

    feat(input): enable user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("───────────────────────\n│ COIN FLIP SIMULATOR │\n───────────────────────\nSelect an action: \n[1] Flip the Coin \n[0] Exit");
+
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
 }
