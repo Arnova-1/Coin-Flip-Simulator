1d65f277840efb0cf189a92f2c2780d5ea5bc68a
Arnova-1 <arnova124@gmail.com>
2025-10-29 10:42:38 +0700

    feat(error): enable error handling for input

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,32 @@ use std::io::Write;
 fn main() {
     println!("───────────────────────\n│ COIN FLIP SIMULATOR │\n───────────────────────\nSelect an action: \n[1] Flip the Coin \n[0] Exit");
 
-    print!("> ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(input) => input,
+            Err(e) => {
+                println!("Failed to read line: {}", e);
+                continue;
+            },
+        };
+        let input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            },
+        };
+
+        match input {
+            0 | 1 => break,
+            _ => {
+                println!("Enter a number between 0 and 1.");
+                continue;
+            },
+        };
+    }
 }
